ec1b0f0d0975a4480d540b9fa873bd7b887ef7a9
SVV-Bonda <122274519+SVV-Bonda@users.noreply.github.com>
2023-02-10 16:11:51 +0530

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormArray } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular 5';
+  myArray = ['val1','val2','val3','val4'];
+
+  constructor(private fb : FormBuilder){
+  }
+
+  ngOnInit(){
+      for(let a = 0; a < this.myArray.length; a++){
+        this.inputsdata.push(this.fb.control(this.myArray[a]));
+      }
+  }
+
+  stepsForm = this.fb.group({
+    inputsdata: this.fb.array([]),
+  });
+
+  get inputsdata() {
+    return this.stepsForm.get('inputsdata') as FormArray;
+  }
+
+  saveData(){
+    console.log(this.inputsdata);
+  }
 }
