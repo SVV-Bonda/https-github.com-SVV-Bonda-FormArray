38e9a4d4bdbda50df9964aa2dcc04a1a814591c2
SVV-Bonda <122274519+SVV-Bonda@users.noreply.github.com>
2023-02-10 17:08:46 +0530

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,32 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormArray } from '@angular/forms';
+import { FormBuilder, FormArray, FormGroup } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular 5';
-  myArray = ['val1','val2','val3','val4'];
+  myArray = ['val1', 'val2', 'val3', 'val4'];
+  stepsForm = FormGroup;
+  myData:any;
 
-  constructor(private fb : FormBuilder){
+  constructor(fb: FormBuilder) {
+    this.stepsForm = fb.group({
+      inputsdata: fb.array([]),
+    });
+
+    for (let a = 0; a < this.myArray.length; a++) {
+      this.inputsdata.push(fb.control(this.myArray[a]));
+    }
   }
-
-  ngOnInit(){
-      for(let a = 0; a < this.myArray.length; a++){
-        this.inputsdata.push(this.fb.control(this.myArray[a]));
-      }
-  }
-
-  stepsForm = this.fb.group({
-    inputsdata: this.fb.array([]),
-  });
 
   get inputsdata() {
     return this.stepsForm.get('inputsdata') as FormArray;
   }
 
-  saveData(){
-    console.log(this.inputsdata);
+  saveData() {
+    console.log(this.stepsForm.value.inputsdata);
+    this.myData = this.stepsForm.value.inputsdata;
   }
 }
